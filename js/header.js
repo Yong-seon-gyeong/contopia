3ece6d01de41a34e1ad31486db90c661559bfe4f
용선경 <133983127+Yong-seon-gyeong@users.noreply.github.com>
2023-12-15 12:07:32 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -10,17 +10,19 @@ $(function () {
 
     });
 
-    var prevScrollTop = 0;
+   var prevScrollTop = 0;
 
-    document.addEventListener("scroll", function () {
-
-        var nowScrollTop = $(window).scrollTop(); //현재 스크롤 위치를 nowScrollTop 에 저장
-
-        if (nowScrollTop > prevScrollTop) { $('header').addClass('active'); }
-        // 스크롤 방향(Down) 내릴때 -> 헤더에 active 클래스 추가
-        else { $('header').removeClass('active'); } // 스크롤 방향(Up) 올릴때 -> 헤더에 active 클래스 제거
-        prevScrollTop = nowScrollTop;  // prevScroll, nowScrollTop 조건 판단 후, 현재 스크롤값을 prevScrollTop 에 저장
-
+    $(window).scroll(function () {
+        var nowScrollTop = $(window).scrollTop();
+    
+        if (nowScrollTop > 100) {
+            if (nowScrollTop > prevScrollTop) {
+                $("#header").addClass("active");
+            } else {
+                $("#header").removeClass("active");
+            }
+            prevScrollTop = nowScrollTop;
+        }
     });
 
     // pc_submenu
